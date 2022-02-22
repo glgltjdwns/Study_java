78b2e020fc0d210c7e36c702833494b7e73c1b1e
user <glgltjdwns@naver.com>
2022-02-22 17:39:06 +0900

    Commit

---
+++ src/com/urdomain/lecture/ch06/ThirdCalculator.java
@@ -0,0 +1,26 @@
+package com.urdomain.lecture.ch06;
+
+public class ThirdCalculator {
+
+	double	a;
+	
+	double 	b;
+	
+	double areaRectangle(double a) {
+		
+		double result1 = a*a;
+		
+		return result1;	
+		
+	}
+	
+	double areaRectangle(double a, double b) {
+		double result2 = a*b;
+		
+		return result2;
+	}
+	
+	
+	
+	
+}
