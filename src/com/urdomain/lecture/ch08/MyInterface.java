36e7a7f9c47fc3d0981af68a67c0e5dd22603a2c
glgltjdwns <glgltjdwns@naver.com>
2022-03-10 17:06:31 +0900

    Commit

---
+++ src/com/urdomain/lecture/ch08/MyInterface.java
@@ -0,0 +1,10 @@
+package com.urdomain.lecture.ch08;
+
+public interface MyInterface {
+	void method1();  	// public abstract
+	
+//	default void method2() {
+//		System.out.println("MyClassB-method1() 실행");
+//	}
+
+}
